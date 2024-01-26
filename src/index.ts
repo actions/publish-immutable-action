cb62dd84505a346d54aed192621889676083c358
ddivad195 <ddivad195@github.com>
2024-01-26 21:31:48 +0000

    remove console log of path

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,5 @@ import { run } from './main'
 import minimist from 'minimist'
 
 const path = minimist(process.argv.slice(2)).path || '.'
-console.log(path)
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 run(path)
