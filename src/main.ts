55e582b23e1f50a9d5178e4fdde26558311b31a7
Edwin Sirko <esirko@github.com>
2024-01-26 13:07:11 -0500

    resolved a TODO item about semver

--- src/main.ts
+++ src/main.ts
@@ -31,7 +31,6 @@ export async function run(pathInput: string): Promise<void> {
     // https://docs.github.com/en/actions/creating-actions/releasing-and-maintaining-actions
     const targetVersion = semver.parse(releaseTag.replace(/^v/, ''))
     if (!targetVersion) {
-      // TODO: We may want to limit semvers to only x.x.x, without the pre-release tags, but for now we'll allow them.
       core.setFailed(
         `${releaseTag} is not a valid semantic version, and so cannot be uploaded as an Immutable Action.`
       )
