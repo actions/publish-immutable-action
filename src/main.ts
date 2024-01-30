621cb8210d437890e6279cbbaa462cb3b19ff8d4
David Daly <ddivad195@github.com>
2024-01-30 14:02:59 +0000

    only run if environment is not ghes

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ import semver from 'semver'
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
-export async function run(pathInput: string): Promise<void> {
+export async function run(): Promise<void> {
   const tmpDirs: string[] = []
 
   try {
